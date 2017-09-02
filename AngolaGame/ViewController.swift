d75b3f6aa77f91233501067644730a52f3fbfdfc
admin <admin@admins-macbook-pro.local>
2017-09-02 13:27:46 -0400

    Initial Commit

---
+++ AngolaGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AngolaGame
+//
+//  Created by admin on 9/2/17.
+//  Copyright © 2017 davCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
